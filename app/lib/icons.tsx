0dade7d00325ba42fc5d86a395d040d92c736811
emirulucay <eux202@gmail.com>
2023-09-06 02:11:50 +0300

    feat: icons & homepage

---
+++ app/lib/icons.tsx
@@ -0,0 +1,31 @@
+export function Logo(props: any) {
+  return (
+    <svg {...props} width="43" height="13" viewBox="0 0 42 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="6.07212" cy="6.50001" r="5.57887" stroke="currentColor" strokeWidth="2" />
+      <circle cx="20.7955" cy="6.50001" r="6.07212" fill="currentColor" />
+      <circle cx="35.5187" cy="6.50001" r="6.07212" fill="currentColor" />
+    </svg>
+  );
+}
+
+export function DownArrow(props: any) {
+  return (
+    <svg {...props} width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9.75 4.625L6 8.375L2.25 4.625" stroke="currentColor" strokeOpacity="1" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
+
+export function Check(props: any) {
+  return (
+    <svg {...props} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M13.3333 4L5.99996 11.3333L2.66663 8"
+        stroke="currentColor"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Header from "./components/Header";
+import RythimSelector from "./components/RythimSelector";
 
 export default function Home() {
   return (
-   <>
-    ASD
-   </>
-  )
+    <>
+      <Header />
+      <div className="mx-auto max-w-[400px] flex flex-col mt-16 border-b border-spaceB-50 pb-1">
+        <div className="flex items-center justify-between px-2">
+          <div className="flex flex-col items-center">
+            <span className="text-white/30 text-xs">PROGRESS</span>
+            <span className="text-[21px] text-white font-medium">001. 1</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-white/30 text-xs">PBM</span>
+            <span className="text-[21px] text-white font-medium">92</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-white/30 text-xs">RYHTIM</span>
+            <span className="text-[21px] text-white font-medium">
+              <RythimSelector />
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
